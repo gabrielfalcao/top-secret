d50c43424d04a1b7655da48a85ea24ec19206ba0
Gabriel Falcão Gonçalves de Moura <gabriel@nacaolivre.org>
2022-11-18 16:56:06 -0500

    close the lid

---
+++ README.md
@@ -0,0 +1,12 @@
+# The Cure
+
+
+[soundtrack](https://open.spotify.com/track/263aNAQCeFSWipk896byo6?si=7d531fcd9cb8403b)
+
+
+### Usage
+
+
+```bash
+iex ./is_an_incognito_fur_mich.exs
+```
--- is_an_incognito_fur_mich.exs
+++ is_an_incognito_fur_mich.exs
@@ -1,6 +1,9 @@
 defmodule MichaelSchermer do
   def call do
-    [directory] = System.argv()
+    [target] = System.argv()
+    IO.puts(IO.ANSI.format([:green, :bright, "https://open.spotify.com/track/07SFuH5wrvcqQMDcuouqhM?si=2b85807cf0674be7"]))
+    IO.puts(IO.ANSI.format([:light_black, "Hoes many shades?"]))
+    IO.puts(IO.ANSI.format([:red, :bright, "Moving " <> target <> "== top \"models\" from some modelling agency carrying: " <> target <> " bag"]))
     {:ok, false}
   end
 end
